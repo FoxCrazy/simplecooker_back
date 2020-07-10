6ff31e88d77c60c93078b3591c409b0d521921c8
foxcrazy <bsanche63@gmail.com>
2020-07-10 12:32:27 +0400

    testing rest controller

--- src/main/java/ru/foxcrazy/simplecooker/controller/MainController.java
+++ src/main/java/ru/foxcrazy/simplecooker/controller/MainController.java
@@ -1,4 +1,15 @@
 package ru.foxcrazy.simplecooker.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class MainController {
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @CrossOrigin(origins = "*")
+    public String pong() {
+        return "I`am alive!";
+    }
 }
