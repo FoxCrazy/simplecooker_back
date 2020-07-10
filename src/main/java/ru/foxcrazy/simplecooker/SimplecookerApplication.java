b0e4af7c5019c52b83fb7a8344ea9fab4e1392b1
foxcrazy <bsanche63@gmail.com>
2020-07-10 08:27:37 +0400

    initialize with spring config

---
+++ src/main/java/ru/foxcrazy/simplecooker/SimplecookerApplication.java
@@ -0,0 +1,13 @@
+package ru.foxcrazy.simplecooker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplecookerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplecookerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/foxcrazy/simplecooker/SimplecookerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.foxcrazy.simplecooker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplecookerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
